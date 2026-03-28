b9b43a3bfe2d70e421cea367247cfaa612c518d6
時崎 ギャラクシー <moebest@outlook.jp>
2026-03-28 06:47:55 +0000

    add S3 feat

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,5 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { Modules } from "@medusajs/framework/utils"
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -13,5 +14,27 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  modules: {
+    [Modules.FILE]: {
+      resolve: "@medusajs/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.FILE_S3_URL,
+              access_key_id: process.env.FILE_S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.FILE_S3_SECRET_ACCESS_KEY,
+              region: process.env.FILE_S3_REGION || "auto",
+              bucket: process.env.FILE_S3_BUCKET,
+              endpoint: process.env.FILE_S3_ENDPOINT,
+              forcePathStyle: true,
+            },
+          },
+        ],
+      },
+    },
+  },
 })
